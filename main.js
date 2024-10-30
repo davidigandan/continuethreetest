d117173183f458a089f7d1bc09fae7ff873927a2
David Igandan <davidigandan@gmail.com>
2024-10-30 11:39:24 +0000

    still working

--- main.js
+++ main.js
@@ -13,65 +13,19 @@ document.body.appendChild( renderer.domElement );
 
 // Contorls
  const controls = new OrbitControls(camera, renderer.domElement);
- 
-const datapoints = [1,2,3] //add more datapoint for demopoint1
- const segments =[]
- for (let i=0; i<datapoints.length; i++) {
-    // segments.push(makeSegment(20,5, Math.PI/4, Math.PI/3));
-    const arrayOfMeshes= makeSegment(20,5, Math.PI/4, Math.PI/4);
-    arrayOfMeshes.forEach(mesh => {
-        segments.push(mesh);
-    })
- }
 
+const segments=[]
+    // segments.push(makeSegment(20,5, Math.PI/4, Math.PI/3));
+    const arrayOfMeshes= makeSegment(20,5, Math.PI/4, Math.PI/3);
+    arrayOfMeshes.forEach(mesh => {
+        segments.push(mesh)});
+    
 segments.forEach( (segment,i)=> {
     // segment.position.set(10*i,0,0);  //uncomment for demopoint2
     scene.add(segment);
 })
 
-// Line 1
-const lineMaterial = new THREE.LineBasicMaterial({ color: "red" });
-const lineGeometry = new THREE.BufferGeometry().setFromPoints([
-    new THREE.Vector3(-120, 10, 0),
-    new THREE.Vector3(11,10, 0)
-]);
-const line = new THREE.Line(lineGeometry, lineMaterial);
-
-// line 2
-const width=5
-const topAngle = Math.PI/4
-const length = 10
-const lineGeometry2 = new THREE.BufferGeometry().setFromPoints([
-    new THREE.Vector3(-120,(width/2*Math.tan(topAngle)) +length , 0),
-    new THREE.Vector3(11,(width/2*Math.tan(topAngle)) +length, 0)
-]);
-const line2 = new THREE.Line(lineGeometry2, new THREE.LineBasicMaterial({ color: "orange" }));
-
-const bottomAngle=Math.PI/3;
-//line3
-const correctYPosition = -(length + (width/2*Math.tan(bottomAngle)) )
-console.log("line says", correctYPosition)
-const lineGeometry3 = new THREE.BufferGeometry().setFromPoints([
-    new THREE.Vector3(-120,correctYPosition, 0),
-    new THREE.Vector3(11,correctYPosition, 0)
-]);
-const line3 = new THREE.Line(lineGeometry3, new THREE.LineBasicMaterial({ color: "yellow" }));
-
-// line4
-const lineGeometry4 = new THREE.BufferGeometry().setFromPoints([
-    new THREE.Vector3(-120, -10, 0),
-    new THREE.Vector3(11,-10, 0)
-]);
-const line4 = new THREE.Line(lineGeometry4, new THREE.LineBasicMaterial({ color: "green" }));
-
-
-// Add the line to the scene
-scene.add(line, line2, line3, line4);
-
-
- scene.add( new THREE.AxesHelper(11) );
-
-
+ 
  function animate() {
 	requestAnimationFrame(animate);
 	controls.update();
--- makeSegment.js
+++ makeSegment.js
@@ -2,70 +2,51 @@ import * as THREE from 'three';
 import { CSG } from 'three-csg-ts';
 
 
-export default function makeSegment(length=20, width=5, topAngle, bottomAngle) {
+export default function makeSegment(length=20, width=5, topAngle, bottomAngle, material = new THREE.MeshBasicMaterial( { color: "green" } )) {
 
+const radius = width/2
+const topExcess = radius*Math.tan(topAngle);
+const bottomExcess = radius*Math.tan(bottomAngle);
 
 // Generate cylinder body
-const precutLength = (width/2*Math.tan(topAngle)) + (width/2*Math.tan(bottomAngle)) + length
-const geometry = new THREE.CylinderGeometry( width/2, width/2, precutLength, 32 );
-const material = new THREE.MeshBasicMaterial( { color: "green" } );
+const precutLength = topExcess + bottomExcess + length
+const geometry = new THREE.CylinderGeometry( radius, radius, precutLength, 36 );
 const cylinder = new THREE.Mesh( geometry, material );
-
-// Remove for 3JS recentring
-const currYPosition = precutLength/2
-const correctYPosition = length/2 + (width/2*Math.tan(topAngle)) //length plus top-excess
-const yAdjustment= correctYPosition-currYPosition
-cylinder.geometry.translate(0,yAdjustment,0)
 
 // Create topCube
 const topCubeWidth = (width/Math.cos(topAngle))
 const topCubeGeometry = new THREE.BoxGeometry(topCubeWidth,topCubeWidth,topCubeWidth);
-const topCubeMaterial = new THREE.MeshBasicMaterial( { color: "red" } );
-const topCube = new THREE.Mesh( topCubeGeometry, topCubeMaterial);
+const topCube = new THREE.Mesh( topCubeGeometry);
 
 
 // Position topCube
-topCube.geometry.rotateZ(topAngle); //rotate topCube 
 topCube.geometry.translate(0,topCubeWidth/2,0)
-const topShift = (length+(width/2*Math.tan(bottomAngle))- (width/2*Math.tan(topAngle))) /2
+topCube.geometry.rotateZ(topAngle); //rotate topCube 
+const topShift = (length+bottomExcess-topExcess) /2
 topCube.geometry.translate(0,topShift,0) 
-
 
 // Top subtraction
 const topTip = CSG.intersect(cylinder, topCube);
-topTip.material = new THREE.MeshBasicMaterial({color: "orange"});
 let slicedCylinder = CSG.subtract(cylinder, topTip);
 
 // Generate bottom cube
 const bottomCubeWidth = (width/Math.cos(bottomAngle))
 const bottomCubeGeometry = new THREE.BoxGeometry(bottomCubeWidth,bottomCubeWidth,bottomCubeWidth);
-const bottomCubeMaterial = new THREE.MeshBasicMaterial( { color: "blue" } );
-const bottomCube = new THREE.Mesh( bottomCubeGeometry, bottomCubeMaterial);
-
+const bottomCube = new THREE.Mesh( bottomCubeGeometry);
 
 // Position bottomCube 
-bottomCube.geometry.rotateZ( bottomAngle) //rotate bottomCube
 bottomCube.geometry.translate(0,-bottomCubeWidth/2,0) 
-const bottomShift = (length+(width/2*Math.tan(topAngle))- (width/2*Math.tan(bottomAngle))) /2
+bottomCube.geometry.rotateZ( bottomAngle) //rotate bottomCube
+const bottomShift = (length + topExcess - bottomExcess) /2
 bottomCube.geometry.translate(0, -bottomShift, 0)
 
-// bottomCube.geometry.translate(0, -length/2 , 0)
-
 // Bottom subtraction
-// const bottomTip = CSG.intersect(slicedCylinder, bottomCube);
-// topTip.material = new THREE.MeshBasicMaterial({color: "black"});
-// slicedCylinder = CSG.subtract(slicedCylinder, bottomTip);
+const bottomTip = CSG.intersect(slicedCylinder, bottomCube);
+slicedCylinder = CSG.subtract(slicedCylinder, bottomTip);
 
-// Scale the cube for the bottom width
-// let scaleFactor = Math.cos(topAngle)/Math.cos(bottomAngle);
-// scaleFactor = 5;
-// topCube.scale.set(scaleFactor,scaleFactor,scaleFactor);
-
-
-// const bottomTip = CSG.intersect(cylinder,topCube);
-// bottomTip.material = topTip.material
-// const finalCylinder = CSG.subtract(slicedCylinder, bottomTip);
-
-return [  topCube,slicedCylinder, bottomCube/*, slicedCylinder ,,toptopCube*/]
+// Move bottom to world origin
+const bottomToOrigin = (precutLength/2) - bottomExcess
+slicedCylinder.geometry.translate(0,bottomToOrigin,0)
+return [slicedCylinder]
 }
 
