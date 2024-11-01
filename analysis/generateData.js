4eddc575c2439a9523c1734ed3a08e93ee8cd845
David Igandan <davidigandan@gmail.com>
2024-11-01 14:06:55 +0000

    safe

---
+++ analysis/generateData.js
@@ -0,0 +1,12 @@
+export function sine(start, stop, step=1) {
+    const dataset=[];
+    for(let i = start; i<stop; i+=step) {
+        dataset.push([i, Math.sin(i)*10])
+    }
+    return dataset;
+} 
+
+// export  function random(seed) {
+//     const dataset=[]
+
+// }
